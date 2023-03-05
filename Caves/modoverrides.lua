27ddc501de7b951ea260e51d0ba4a222a311c29b
alo <anlongtu19@gmail.com>
2023-03-05 20:02:03 +0800

    feat: mod added and config

---
+++ Caves/modoverrides.lua
@@ -0,0 +1,58 @@
+return {
+  ["workshop-1185229307"]={
+    configuration_options={
+      [""]="",
+      DAMAGE_NUMBERS=true,
+      DAMAGE_RESISTANCE=true,
+      FRAME_PHASES=true,
+      HORIZONTAL_OFFSET=0,
+      NOEPIC=false,
+      NONOEPIC=false,
+      WETNESS_METER=false 
+    },
+    enabled=true 
+  },
+  ["workshop-1226903961"]={
+    configuration_options={
+      bossstrengthen=false,
+      bossstrengthen_super=false,
+      checkcoin=false,
+      checkstart=true,
+      trinketedible=false 
+    },
+    enabled=true 
+  },
+  ["workshop-1595631294"]={
+    configuration_options={
+      BundleItems=false,
+      ChangeSkin=true,
+      Digornot=false,
+      DragonflyChest=false,
+      Icebox=false,
+      SaltBox=false 
+    },
+    enabled=true 
+  },
+  ["workshop-2078243581"]={
+    configuration_options={ Blue=0, Display="target", Green=0, Projectile=true, Red=1, Type="hit" },
+    enabled=true 
+  },
+  ["workshop-2902364746"]={ configuration_options={ mode="Share" }, enabled=true },
+  ["workshop-362175979"]={ configuration_options={ ["Draw over FoW"]="disabled" }, enabled=true },
+  ["workshop-375850593"]={ configuration_options={  }, enabled=true },
+  ["workshop-666155465"]={
+    configuration_options={
+      chestB=-1,
+      chestG=-1,
+      chestR=-1,
+      display_hp=-1,
+      food_estimation=-1,
+      food_order=0,
+      food_style=0,
+      lang="auto",
+      show_food_units=-1,
+      show_uses=-1 
+    },
+    enabled=true 
+  } 
+}
